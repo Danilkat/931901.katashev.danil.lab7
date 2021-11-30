ad1177092e3ceb0de7dd50ea1a2cac4793a53a69
Danil Katashev <katashnya@gmail.com>
2021-12-01 01:54:45 +0700

    Значительно  укоротил код

--- script.js
+++ script.js
@@ -1,28 +1,15 @@
-var topvar;
-var left;
-var size;
-let count = 0;
-
-function getNumbers() {
-  size = 50 + Math.random() * (Math.min(window.innerWidth, window.innerHeight) / 3 - 50);
-  topvar = Math.random() * (window.innerHeight - 2*size);
-  left = Math.random() * (window.innerWidth - 2*size);
-}
-
 function createFigure(e) {
-  e = e < 0 ? 0 : e;
   for (let i = 0; i < document.querySelector(".amount").value; i++) {
-    getNumbers();
+    size = 50 + Math.random() * (Math.min(window.innerWidth, window.innerHeight) / 3 - 50)
     style = {
       width: size + "px",
       height: size + "px",
-      top: topvar + "px",
-      left: left + "px"
+      top: Math.random() * (window.innerHeight - 2*size) + "px",
+      left: Math.random() * (window.innerWidth - 2*size) + "px"
     }
     element = document.createElement("div");
     element.className = e;
-    element.id = "figure" + count++;
-    console.log(style);
+    element.tabIndex = 0;
     for ( var key in style) {
       element.style[key] = style[key];
     }
